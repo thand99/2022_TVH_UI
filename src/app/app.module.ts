e8b328b602e98c8879a5b3d43638a3dd43223ca5
Vuyelo <chaukevn4@gmail.com>
2022-03-03 16:22:31 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { RewardsComponent } from './rewards/rewards.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    RewardsComponent,
+    GalleryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  countDownDate = new Date();
+
+  days:any = 45;
+  hours:any = 20;
+  min:any = 35;
+  secs:any = 3;
+  daysN = "Days";
+  hoursN = "Hours";
+  minN:any ="Minutes";
+  secsN:any ="Seconds"
+
+
+  exp:any ="Competition Expired";
+
+  x = setInterval(()=>{
+
+    var futureDate = new Date("March 27, 2022 01:31:24 ").getTime();
+    var today = new Date().getTime();
+    var distance = futureDate - today;
+
+    this.days = Math.floor(distance/(1000 * 60 * 60 * 24 ));
+    this.hours = Math.floor((distance %(1000 * 60 * 60 * 24)) /(1000 * 60 * 60));
+    this.min = Math.floor((distance %(1000 * 60 * 60)) /(1000 * 60 ));
+    this.secs = Math.floor((distance %(1000 * 60 )) /(1000 ));
+
+    if(distance < 0)
+    {
+      clearInterval(this.x)
+      this.days =" Expired";
+      this.hours =" ";
+      this.min = " ";
+      this.secs =" ";
+      this.daysN = " ";
+      this.hoursN = " ";
+      this.minN = " ";
+      this.secsN = " ";
+      this.exp;
+
+
+    }
+
+
+
+  }, 1000)
+
+
+
+}
