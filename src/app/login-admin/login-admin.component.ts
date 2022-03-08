e316892578a86e7b8e7311bbfd54dc987a178242
mzwendoda <95077324+mzwendoda@users.noreply.github.com>
2022-03-09 01:20:34 +0200

    Sanele

---
+++ src/app/login-admin/login-admin.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {ApiserviceService} from '../apiservice.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login-admin',
+  templateUrl: './login-admin.component.html',
+  styleUrls: ['./login-admin.component.css']
+})
+export class LoginAdminComponent implements OnInit {
+  names :string =''
+  password:string =''
+  email:string =''
+  constructor(private service:ApiserviceService, private route:Router) { }
+
+  ngOnInit(): void {
+
+
+
+  
+  }
+
+  clickhandle(){
+
+    this.service.getAllData({"emal":this.email, "passw": this.password,"fullnam": this.names }).subscribe((res)=>{
+       
+      console.log(res.message);
+
+      if(res.message =="login Successful")
+      {
+        alert(res.message);
+          this.route.navigate(["admin"])
+      }
+      else{
+
+        alert(res.message);
+      }
+    });
+    
+    
+  }
+
+}
