1c1747fabbf35282878d673fc84fc1c14fe94f5a
mzwendoda <95077324+mzwendoda@users.noreply.github.com>
2022-03-30 23:53:19 +0200

    Sanele

--- src/app/login-admin/login-admin.component.ts
+++ src/app/login-admin/login-admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiserviceService} from '../apiservice.service';
 import { Router } from '@angular/router';
+import {ReactiveFormsModule, FormsModule, Validators, FormGroup, FormControl} from '@angular/forms';
+import {  EventEmitter, Input, Output } from '@angular/core';
 
 
 @Component({
@@ -16,14 +18,35 @@ export class LoginAdminComponent implements OnInit {
 
   ngOnInit(): void {
 
+    
+    this.loginForm = new FormGroup({
+
+      "email": new FormControl(null, [Validators.required, Validators.email]),///
+      "password": new FormControl(null,[Validators.required, Validators.minLength(6), Validators.pattern('[a-zA-z]*')]), ///
+
+       //"pdf": new FormControl(null, [Validators.required, requiredFileType('pdf')])
+
+    })
+
 
 
   
   }
 
+  get emails(){return this.loginForm.get('email');}
+  get passwords(){return this.loginForm.get('password');}
+
+
+
+  loginForm:any;
+
+  public emal:string =''
+  public passw:string =''
+
+
   clickhandle(){
 
-    this.service.getAllData({"emal":this.email, "passw": this.password,"fullnam": this.names }).subscribe((res)=>{
+    this.service.getAllData({"emal":this.emal, "passw": this.password }).subscribe((res)=>{
        
       console.log(res.message);
 
--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiserviceService} from '../apiservice.service';
 import { Router } from '@angular/router';
-import {ReactiveFormsModule, FormsModule, Validators, FormGroup, FormControl} from '@angular/forms';
+import {ReactiveFormsModule, FormsModule, Validators, FormGroup, FormControl, FormBuilder} from '@angular/forms';
 import {  EventEmitter, Input, Output } from '@angular/core';
 
 
 
+interface institutess {
+  value: string;
+  viewValue: string;
+}
 
+interface genders {
+  value: string;
+  viewValue: string;
+}
+
+interface skillss {
+  value: string;
+  viewValue: string;
+}
 
 
 @Component({
@@ -23,26 +36,58 @@ import {  EventEmitter, Input, Output } from '@angular/core';
 export class RegistrationFormComponent implements OnInit {
   
 
+  name = 'Angular';
+  
+  
+  //reactiveForm: registerForm;
+  //registerForm: FormGroup;
+  submitted: boolean = false;
 
-  constructor(private service:ApiserviceService, private route:Router) { }
+  constructor(private service:ApiserviceService, private route:Router, private formBuilder: FormBuilder) {
 
+   }
 
+ // registerForm!: FormGroup;
+
+  institutes: institutess[] = [
+    {value: 'tut', viewValue: 'Tshwane University Of Technology'},
+    {value: 'up', viewValue: 'University Pretoria'},
+    {value: 'smu', viewValue: 'Sefako Makgatho University'},
+    {value: 'unisa', viewValue: 'Unisa'}
+  ];
+
+  genders: genders[] = [
+    {value: 'M', viewValue: 'Male'},
+    {value: 'F', viewValue: 'Female'},
+
+  ];
+
+    skill: skillss[] = [
+    {value: 'An', viewValue: 'Analyst'},
+    {value: 'Prgm', viewValue: 'Programmer'},
+    {value: 'Graphics', viewValue: 'Graphic designer'},
+    {value: 'Maketr', viewValue: 'Marketer'},
+    
+
+  ];
+ 
  
 
-
   ngOnInit(): void {
+
 
 
     this.registerForm = new FormGroup({
 
       "names": new FormControl(null, [Validators.required, Validators.pattern('[a-zA-z]*')]),///
+      "surname": new FormControl(null, [Validators.required, Validators.pattern('[a-zA-z]*')]),
+      "genders": new FormControl(null,Validators.required ),
+      "dates": new FormControl(null,Validators.required ),
       "email": new FormControl(null, [Validators.required, Validators.email]),///
-      "password": new FormControl(null,Validators.required ), ///
-      "mobileNumber": new FormControl(null,[Validators.required, Validators.pattern('[0-9]*')] ),
       "institutes": new FormControl(null,Validators.required ),
+      "academic": new FormControl(null,Validators.required ),
       "skill": new FormControl(null,Validators.required ),
-      "statuss": new FormControl(null,Validators.required )
-
+      "mobileNumber": new FormControl(null,[Validators.required, Validators.pattern('[0-9]*')] ),
     })
   }
 
@@ -52,85 +97,128 @@ export class RegistrationFormComponent implements OnInit {
 
 
   public names :string =''
+  public surname:string =''
   public gender:string =''
   public dateBirth:string =''
   public stud_email:string =''
-  public password:string =''
   public institute:string =''
   public academicRecord:string =''
   public  mNumber:string =''
   public skills:string =''
-  public status:string =''
+
+ 
 
   registerForm:any; ////
 
   //////////
 
-
+  public hold :string =''
 
 
   ///submit Function
 
-  submitData()
+  get namess(){return this.registerForm.get('names');}
+  get surnames(){return this.registerForm.get('surname');}
+  get genderss(){return this.registerForm.get('genders');}
+  get datess(){return this.registerForm.get('dates');}
+  get emails(){return this.registerForm.get('email');}
+  get institutess(){return this.registerForm.get('institutes');}
+  get academicss(){return this.registerForm.get('academic');}
+  get mobilenumber(){return this.registerForm.get('mobileNumber');}
+  get skil(){return this.registerForm.get('skill');}
+ 
+
+ 
+  
+  
+ // get pdfs(){return this.registerForm.get('pdf');}
+
+
+ get f(){return this.registerForm.controls}
+
+
+
+
+
+
+  clickhandle()
   {
-    console.log(this.registerForm.value);
 
-    if(this.registerForm.valid)
+    this.submitted = true;
+
+    
+    
+
+    if(this.registerForm.invalid)
     {
-       alert('Thank you, your application submitted');
-       this.registerForm.reset();
+     // this.hold = this.registerForm.invalid;
+      console.log(this.hold);
+      console.log('invalid');
 
-       this.route.navigate(["home"])
+      
+      
+      return;
+    }else 
+    {
+
+      //this.hold = this.registerForm.valid;
+
+      console.log(this.hold);
+      console.log('not valid');
+
+      this.service.getRegistration({"fullName":this.names, "surname":this.surname, "gender": this.gender,"dateOfBirth": this.dateBirth,"emailAddress": this.stud_email,"institution": this.institute,"academicRecord": this.academicRecord,"mobileNumber": this.mNumber,"skills": this.skills }).subscribe((res)=>{
+        console.log(this.names);
+        console.log(this.surname);
+        console.log(this.gender);
+        console.log(this.dateBirth);
+        console.log(this.stud_email);
+        console.log(this.institute);
+        console.log(this.academicRecord);
+        console.log(this.mNumber);
+        console.log(this.skills);
+        console.log(res.message);
+
+        if(res.message =="Application Successfully submited")
+        {
+            alert(res.message);
+            this.registerForm.reset();
+            this.route.navigate(["home"])
+        }
+        else{
+          alert(res.message);
+        }
+      });
+
+      
     }
 
+    
+
+
   }
 
-  get namess(){return this.registerForm.get('names');}
-  get emails(){return this.registerForm.get('email');}
-  get passwords(){return this.registerForm.get('password');}
-  get mobilenumber(){return this.registerForm.get('mobileNumber');}
-  get inst(){return this.registerForm.get('institutes');}
-  get skil(){return this.registerForm.get('skill');}
-  get statess(){return this.registerForm.get('statuss');}
   
-
-
-  clickhandle(){
-
-    console.log(this.names);
-
-    this.service.getRegistration({"names":this.names, "gender": this.gender,"dateBirth": this.dateBirth,"stud_email": this.stud_email ,"password": this.password,"institute": this.institute,"academicRecord": this.academicRecord,"mobileNumber": this.mNumber,"skill": this.skills,"status": this.status }).subscribe((res)=>{
-       
-      console.log(res.message);
-
-
-
-      if(res.message =="Application Successfully submited")
-      {
-          alert(res.message);
-          this.route.navigate(["home"])
-          
-      }
-      else{
-
-        alert(res.message);
-      }
-    });
-    
-    
-  }
-
-
-
-  }
+  
+}
 
 
 
 
+  interface HTMLInputEvent extends Event {
+    target: HTMLInputElement & EventTarget;
+}
+ 
 
 
 
 
+ 
+function requiredFileType(arg0: string): import("@angular/forms").ValidatorFn {
+  throw new Error('Function not implemented.');
+}
 
-
-
+export class institute
+{
+    public ids:number | undefined;
+    public institute:string | undefined;
+} 
