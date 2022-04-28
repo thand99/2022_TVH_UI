679d01daab3c116e17f2997bcebcfe10fa332a3f
215656322 <61270620+215656322@users.noreply.github.com>
2022-04-28 14:18:42 +0200

    updated admin partcipant

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { dialogModel } from './dialog.model';
 
 @Component({
   selector: 'app-dialog',
@@ -8,41 +6,10 @@ import { dialogModel } from './dialog.model';
   styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent implements OnInit {
-  formValue!: FormGroup;
-  dialogModelLeader: dialogModel = new dialogModel();
-  api: any;
 
-  constructor(private formBuilder:FormBuilder) { }
-  
+  constructor() { }
 
   ngOnInit(): void {
-    this.formValue = this.formBuilder.group({
-      firstname: [''],
-      surname: [''],
-      image: [''],
-      category: [''],
-      description: ['']
-    })
   }
-
-  postLeaderDetails(){
-    this.dialogModelLeader.name = this.formValue.value.name;
-    this.dialogModelLeader.surname = this.formValue.value.surname;
-    this.dialogModelLeader.image = this.formValue.value.image;
-    this.dialogModelLeader.category = this.formValue.value.category;
-    this.dialogModelLeader.description = this.formValue.value.description;
-
-
-    this.api.postLeader(this.dialogModelLeader).subscrribe((res: any)=>{
-      console.log(res);
-      alert("Leader Record Addes Succsesfully !");
-      this.formValue.reset();
-    },
-      (err: any)=>{
-      alert("something went wrong !");
-    })
-
-  }
-
 
 }
